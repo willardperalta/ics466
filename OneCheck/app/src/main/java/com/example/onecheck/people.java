0f2ce4097ff7cd55afa4ae7fc1c59df7670d3abb
Willard Peralta <willardp808@gmail.com>
2020-02-23 18:16:49 -1000

    test listview and create mockups

--- OneCheck/app/src/main/java/com/example/onecheck/people.java
+++ OneCheck/app/src/main/java/com/example/onecheck/people.java
@@ -5,15 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class People extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String[] myDataset = new String[] {"bob", "michelle", "jake"};
+        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+                "WebOS","Ubuntu","Windows7","Max OS X"};
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people);
-
 
     }
 

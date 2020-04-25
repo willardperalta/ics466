5bfba1ea06fd7ccd3a57ab2019b50996cab715a8
Willard Peralta <willardp808@gmail.com>
2020-04-24 16:30:02 -1000

    a

--- OneCheck/app/src/main/java/com/example/onecheck/ReceiptActivity.java
+++ OneCheck/app/src/main/java/com/example/onecheck/ReceiptActivity.java
@@ -6,22 +6,17 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ReceiptActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void launchHistoryActivity(View view) {
-        Intent intent = new Intent(this, CheckHistory.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_receipt);
     }
 
 
     public void launchCombinedActivity(View view) {
-        Intent intent = new Intent(this, ReceiptActivity.class);
+        Intent intent = new Intent(this, CombinedActivity.class);
         startActivity(intent);
     }
 }

2499f98c4937730f8ca8404b8f80199265c9729e
Christopher Nishimura <42785017+chris-nishimura@users.noreply.github.com>
2020-04-24 16:28:10 -1000

    trying to display on ui

--- OneCheck/app/src/main/java/com/example/onecheck/CheckHistory.java
+++ OneCheck/app/src/main/java/com/example/onecheck/CheckHistory.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class CheckHistory extends AppCompatActivity {
     private CheckViewModel mcheckViewModel;
@@ -16,9 +20,20 @@ public class CheckHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_history);
 
-       RecyclerView recyclerView = findViewById(R.id.recyclerView);
-       final CheckListAdapter adapter = new CheckListAdapter(this);
-       recyclerView.setAdapter(adapter);
-       recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final CheckListAdapter adapter = new CheckListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mcheckViewModel = new ViewModelProvider(this, null).get(CheckViewModel.class);
+
+        mcheckViewModel.getAllChecks().observe(this, new Observer<List<Check>>() {
+            @Override
+            public void onChanged(List<Check> checks) {
+                adapter.setChecks(checks);
+            }
+        });
+
+
     }
 }

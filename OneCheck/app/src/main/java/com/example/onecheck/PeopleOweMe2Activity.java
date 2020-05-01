df031151a30dd9284efe852b383d09d29b769a66
Willard Peralta <willardp808@gmail.com>
2020-04-30 17:07:25 -1000

    PeopleOweMe Activity

--- OneCheck/app/src/main/java/com/example/onecheck/PeopleOweMe2Activity.java
+++ OneCheck/app/src/main/java/com/example/onecheck/PeopleOweMe2Activity.java
@@ -1,14 +1,11 @@
 package com.example.onecheck;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,8 +13,9 @@ import androidx.recyclerview.widget.RecyclerView;
 public class PeopleOweMe2Activity extends AppCompatActivity {
 
     MyRecyclerViewAdapter adapter;
-
     HashMap<String, String> nameAndPrices = new HashMap<>(); //stores the name and corresponding price
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> resultCosts;
     ArrayList<String> finalResults = new ArrayList<>();
     double evenlyDistributedTaxAndTip;
     Button doneButton;
@@ -27,19 +25,57 @@ public class PeopleOweMe2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people_owe_me2);
 
+        /*This was used to get data using intents
+        Intent listIntent = getIntent();
+        nameAndPrices = (HashMap<String, String>) listIntent.getSerializableExtra("nameAndPrices");
+        names = (ArrayList<String>) listIntent.getSerializableExtra("key");
+        evenlyDistributedTaxAndTip = listIntent.getDoubleExtra("evenlyDistributedTaxAndTip", evenlyDistributedTaxAndTip);
+        resultCosts = new ArrayList<>(names.size()); //initialize the costs array to be as big as the names array
+         */
+
+
+
+
+
+
+
+
+
+        /*Need to populate nameAndPrices hashmap, names ArrayList, evenlyDistributedTaxAndTip double, and resultCosts ArrayList
+        in order to fill in recyclerview and to make below code to work, might need to do some adjustments though
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //set text to show what evenlyDistributedTaxAndTip is
         TextView textView = (TextView) findViewById(R.id.distributedtaxandtip);
-        evenlyDistributedTaxAndTip = 3.22;
         textView.setText("$" + String.format("%.2f", evenlyDistributedTaxAndTip));
         doneButton = (Button) findViewById(R.id.doneButton);
 
-        //example data
-        finalResults.add("John: $13.99");
-        finalResults.add("Amy: $19.22");
-        finalResults.add("Jonathan: $4.76");
-        finalResults.add("Adam: $21.21");
-        finalResults.add("Samantha: $19.32");
-        finalResults.add("Jake: $23.12");
+        //populate resultCosts array with prices from the hashmap
+        for(int i = 0; i < names.size(); i++) {
+            System.out.println(i);
+            System.out.println(names.get(i));
+            resultCosts.add(nameAndPrices.get(names.get(i))); //using add() method get the string for the price
+        }
+
+        //put together results in one array
+        for(int i = 0; i < names.size(); i++) {
+            finalResults.add(i, names.get(i) + ": $" + String.format("%.2f", Double.parseDouble(resultCosts.get(i))));
+        }
 
         // Display name: $price
         RecyclerView recyclerViewNames = findViewById(R.id.resultsnames);
@@ -52,15 +88,19 @@ public class PeopleOweMe2Activity extends AppCompatActivity {
                 {
                     public void onClick(View view)
                     {
-                        finish();
+                        launchMainActivity(view);
                     }
                 });
 
+    }
+
+    //onclick method to go back to the previous activity and edit the prices
+    public void goBackToPeopleAndItems(View view) {
+        finish(); //this method just goes back to previous activity
     }
 
     public void launchMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
